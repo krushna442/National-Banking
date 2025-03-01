900d2e86fe479c24cb1d4529258f47a8a0f158f7
krushna <krushnch442@gmail.com>
2025-03-01 16:08:15 +0530

    add super admin

--- routes/superAdmin.js
+++ routes/superAdmin.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import Admin from "../models/Adminmodel";
+import Admin from "../models/Adminmodel.js";
 
 const router = express.Router();
 
